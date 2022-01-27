15e83228f96926dede7deaad1c86fd775642ed85
Dirtarih <bastricov83@gmail.com>
2022-01-27 15:02:00 +0500

    Кейс 2.4
    Кейс 2.4

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_course_2/case-2-2-pizza.dart';
+import 'package:pizza_course_2/case-2-3-navigation.dart';
+
+
+void main() {
+  //Чтобы запустить case-2-2-pizza - используйте конструктор MyApp
+  //Чтобы запустить case 2-3-navigation - используйте конструктор NavigationDrawerScreen
+  runApp(const MyApp());
+}
